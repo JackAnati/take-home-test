95fc0919817f628923201b4be874d9967ef33340
JackAnati <Anatijack0@gmail.com>
2022-08-15 19:41:52 +0200

    updated solution

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
-// Your solution here
+function calc(){
+
+    num1 = parseInt(form1.num1.value);
+    num2 = parseInt(form1.num2.value);
+   
+    if (op1.checked){
+        
+        answer = parseInt(num1 + num2);
+        form1.answer.value=answer;
+        
+    }
+    else if(op2.checked){
+        answer = parseInt(num1 * num2);
+        form1.answer.value=answer;
+
+    }
+    else if(op3.checked){
+        answer = parseInt(num1 / num2);
+       form1.answer.value=answer;
+
+    }
+    else if(op4.checked){
+        answer = parseInt(num1 - num2);
+       form1.answer.value=answer;
+
+    }
+
+
+}
